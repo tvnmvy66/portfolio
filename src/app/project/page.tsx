8a8e50d3d1b1c55ef05ea426cd812f2cdcdbf39c
Tanmay Dongare <tvnmvy@gmail.com>
2025-06-05 13:15:53 +0530

    added projects dynamically from github repos

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -1,10 +1,70 @@
-'use client';
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from "@/components/Card"
+import Link from "next/link";
+
+type Repo = {
+  id: number;
+  name: string;
+  html_url: string;
+  description: string | null;
+};
+
+type GitHubReposProps = {
+  username: string;
+};
 
 export default function Page() {
+
+  const [repos, setRepos] = useState<Repo[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchRepos = async () => {
+      try {
+        const res = await fetch(`https://api.github.com/users/tvnmvy66/repos`);
+        if (!res.ok) throw new Error("GitHub API error");
+        const data = await res.json();
+        setRepos(data);
+      } catch (err) {
+        setError((err as Error).message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchRepos();
+  }, []);
+
+  if (loading) return <p>Loading Projects</p>;
+  if (error) return <p>Error: {error}</p>;
+
   return (
     <>
-    PROJECT
+    <div className='grid grid-cols-3 gap-6 text-center w-[90vw] mx-auto mt-14 md:mt-10 mb-10 '>
+      {repos.map((repo) => (
+      <div key={repo.name} className='col-span-3 md:col-span-1 h-40 w-full '>
+        <Link href={repo.html_url}>
+        <Card className="w-full h-full">
+          <CardHeader>
+          <CardTitle className='text-xl md:text-2xl'>{repo.name}</CardTitle>
+          {/* <CardDescription>HTML, CSS, JavaScript, React, NextJs</CardDescription> */}
+          </CardHeader>
+          <CardContent>
+            {repo.description}
+          </CardContent>
+        </Card>
+        </Link>
+      </div>
+      ))}
+    </div>
     </>
   );
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,51 +4,72 @@
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/Avatar"
 import Link from 'next/link';
+import Image from 'next/image';
 
 const Footer: React.FC = () => {
-  return (
-    <>
-        <div className=' text-black pt-4 md:flex md:justify-center'>
-            <div className="grid grid-cols-4 gap-2 md:gap-1 mx-5 text-center md:w-80">
-                <div className="col-span-4 h-10">
-                    <div className='flex justify-center items-center'>
-                        <Avatar className="h-10 w-10 md:h-9 md:w-9">
-                            <AvatarImage src="https://github.com/tvnmvy66.png" />
-                            <AvatarFallback>CN</AvatarFallback>
-                        </Avatar>
-                        <p className='text-lg md:text-lg pl-2'>Tanmay Dongare</p>
+    return (
+        <>
+            <div className=' text-black pt-4 md:flex md:justify-center'>
+                <div className="grid grid-cols-4 gap-2 md:gap-1 mx-5 text-center md:w-80">
+                    <div className="col-span-4 h-10">
+                        <div className='flex justify-center items-center'>
+                            <Avatar className="h-10 w-10 md:h-9 md:w-9">
+                                <AvatarImage src="https://github.com/tvnmvy66.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                            <p className='text-lg md:text-lg pl-2'>Tanmay Dongare</p>
+                        </div>
+                    </div>
+                    <div className="col-span-2 md:col-span-1 h-8">
+                        <Link href="/">
+                            Home
+                        </Link>
+                    </div>
+                    <div className="col-span-2 md:col-span-1 h-8">
+                        <Link href="/project">
+                            Project
+                        </Link>
+                    </div>
+                    <div className="col-span-2 md:col-span-1 h-8">
+                        <Link href="/blog">
+                            Blog
+                        </Link>
+                    </div>
+                    <div className="col-span-2 md:col-span-1 h-8">
+                        <Link href="/contact">
+                            Contact
+                        </Link>
+                    </div>
+                    <div className="col-span-4 h-15 flex justify-center items-center list-none gap-4">
+                        <li><Link href="https://x.com/tvnmvy_"><Image
+                            src="./twitter.svg"     // For public folder images
+                            alt="github icon - redirects to tanmays github page"       // Always add alt for accessibility & SEO
+                            width={20}             // Required
+                            height={20}             // Optional: loads image ASAP
+                        /></Link></li>
+                        <li><Link href="gmail.com"><Image
+                            src="./mail.svg"     // For public folder images
+                            alt="github icon - redirects to tanmays github page"       // Always add alt for accessibility & SEO
+                            width={20}             // Required
+                            height={20}             // Optional: loads image ASAP
+                        /></Link></li>
+                        <li><Link href="linkedin.com"><Image
+                            src="./linkedin.svg"     // For public folder images
+                            alt="github icon - redirects to tanmays github page"       // Always add alt for accessibility & SEO
+                            width={20}             // Required
+                            height={20}             // Optional: loads image ASAP
+                        /></Link></li>
+                        <li><Link href="http://github.com/tvnmvy66"><Image
+                            src="./github.svg"     // For public folder images
+                            alt="github icon - redirects to tanmays github page"       // Always add alt for accessibility & SEO
+                            width={20}             // Required
+                            height={20}             // Optional: loads image ASAP
+                        /></Link></li>
                     </div>
                 </div>
-                <div className="col-span-2 md:col-span-1 h-8">
-                    <Link href="/">
-                        Home
-                    </Link>
-                </div>
-                <div className="col-span-2 md:col-span-1 h-8">
-                    <Link href="/project">
-                        Project
-                    </Link>
-                </div>
-                <div className="col-span-2 md:col-span-1 h-8">
-                    <Link href="/blog">
-                        Blog
-                    </Link>
-                </div>
-                <div className="col-span-2 md:col-span-1 h-8">
-                    <Link href="/contact">
-                        Contact
-                    </Link>
-                </div>
-                <div className="col-span-4 h-10 flex justify-center items-center list-none">
-                    <li>⚓</li>
-                    <li>⚓</li>
-                    <li>⚓</li>
-                    <li>⚓</li>
-                </div>
             </div>
-        </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Footer;
